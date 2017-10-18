0284337cfc1ae21168ba472562370e0475a5dfd5
Ben Bolker <bbolker@gmail.com>
2017-10-18 11:36:05 -0400

    BMB graphs

--- Graphing_MM_Algae.R
+++ Graphing_MM_Algae.R
@@ -1,8 +1,8 @@
 library(ggplot2)
 library(nlmrt)
 
-load(cleandat.R)
-load(Mich_Death_functions.r)
+source("cleandat.R")
+source("Mich_Death_functions.R")
 
 ch2 <- data.frame(ch1)
 #ch2$time <- seq(1,11)
@@ -98,3 +98,74 @@ for(i in 2:nrow(test_dat)) {
 plot(test_dat)
 test_dat[11, ]
 
+
+##
+library(tibble)
+library(dplyr)
+library(ggplot2); theme_set(theme_bw())
+
+cfun1 <- function(dd,valname="chl") {
+    ## add day as a column and make sure it's numeric
+    dd2 <- rownames_to_column(as.data.frame(dd),"day") %>%
+        mutate(day=as.numeric(as.character(day)))
+    ## convert to long form (except for day): put trial in the right order
+    dd3 <- gather(dd2,key=trial,value=val,-day) %>%
+        mutate(trial=factor(trial,levels=unique(trial)))
+    names(dd3)[3] <- valname ## ugh
+    return(dd3)
+}
+
+cdat <- full_join(cfun1(ch1),cfun1(nh1,"nh4"))
+
+trt_tab <- data.frame(trial=factor(1:30,levels=1:30),
+                      nutr=rep(c(0.5,3,9,27,54,108),each=5))
+cdat <- full_join(cdat,trt_tab)
+
+cdat <- cdat %>%
+    group_by(trial) %>%
+    mutate(chl_percap_change=log(chl/lag(chl)))
+
+ggplot(cdat,aes(day,chl))+
+    geom_line(aes(colour=factor(nutr),group=trial),alpha=0.6)+
+    geom_smooth(aes(group=nutr,colour=factor(nutr)),alpha=0.2)
+
+ggplot(cdat,aes(day,chl_percap_change))+
+    geom_line(aes(group=trial,colour=factor(nutr)),alpha=0.3)+
+    geom_smooth(aes(group=nutr,colour=factor(nutr),fill=factor(nutr)),alpha=0.1)+
+    geom_hline(aes(yintercept=0),lty=2)
+
+
+cdat <- cdat %>% mutate(high_nutr=nutr>=27)
+ggplot(cdat,aes(chl,chl_percap_change))+
+    geom_point(aes(colour=factor(nutr)))+
+    geom_hline(aes(yintercept=0),lty=2)+
+    geom_path(aes(colour=factor(nutr),group=trial),alpha=0.2)+
+    ## geom_smooth(## method="gam",
+                ## formula=y~s(x,k=10),
+    ##   aes(group=nutr,colour=factor(nutr),fill=factor(nutr)),alpha=0.1)+
+    facet_wrap(~high_nutr,nrow=1,scale="free_x")
+
+
+ggplot(subset(cdat,chl<=300),
+              aes(chl,chl_percap_change))+
+    geom_point(aes(colour=factor(nutr)))+
+    geom_hline(aes(yintercept=0),lty=2)+
+    ## geom_path(aes(colour=factor(nutr),group=trial),alpha=0.2)+
+    geom_smooth(## method="gam",
+                ## formula=y~s(x,k=10),
+       aes(group=nutr,colour=factor(nutr),fill=factor(nutr)),alpha=0.1)
+
+
+
+ggplot(cdat,aes(nh4,chl_percap_change))+
+    geom_point(aes(colour=factor(nutr)))+
+    scale_x_log10()
+
+
+ggplot(subset(cdat,chl_percap_change>0),
+       aes(nh4,chl))+
+    geom_point(aes(size=chl_percap_change,colour=factor(nutr)),
+               alpha=0.3)+
+    geom_path(aes(group=trial,colour=factor(nutr)),alpha=0.2)+
+    scale_x_log10()+
+    scale_size(range=c(1,10))
