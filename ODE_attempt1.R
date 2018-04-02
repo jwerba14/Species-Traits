d2c040bc90ae823e22d7b43d644642efc0ece1be
jwerba14 <werbaj@mcmaster.ca>
2018-04-02 14:06:43 -0400

    some dataframes

---
+++ ODE_attempt1.R
@@ -0,0 +1,67 @@
+# attempt to write system of differential equations
+
+library(deSolve)
+# state variables given completely arbitary values but wanted to get names on them
+
+state <- c(ammonium = 5,
+           nitrate = 5,
+           daph_j = 10,
+           daph_a = 10
+           cerio = 20,
+           algae = 50
+  
+)
+#parameters- will need parameters for each function
+parameters <- c(x,x,x)
+
+#need saturation function - will need to have different parameters for each saturation funciton in ODE
+sat_fun <- function(a, s, p) {
+  
+  a * p / (s + p)
+
+}
+
+
+
+# write ODE- return has to be in same order as state variable equations are listed
+
+> full_equations <-function(t, state, parameters) {
+  with(as.list(c(state, parameters)),{
+  # rate of change
+        # ammonium is added from release by all 3 zoo species (each times a constant?)   
+      dammonium <- daph_a*sat_fun(a,b,algae)*r + daph_j*sat_fun(c,d,algae)*g+ 
+        cerio*sat_fun(e,f,algae)*y +
+        # also add a constant released based on density dependent death
+        daph_j*deathj*cj + daph_a*deatha*cda + cerio*deathc*cc - algae*deatha*ca-
+        # lose from uptake and a density dependent (?) from air
+        algae*sat_fun(t,h,ammonium) - cammonium*ammonium
+      
+      
+        # nitrate is added by nitrification from ammonium at a constant rate
+      dnitrate <- n*ammonium -
+        # nitrate is loss to air at a density dependent rate and from uptake by algae
+        algae*sat_fun(p,q, nitrate) - cnitrate*nitrate
+      
+        #juvenile daphia are added by density dependent (large daphnia) birth      
+      ddaph_j <- sat_fun(z,w,algae)*daph_a -
+        #loss through death and growth -where they reach a certain threshold size --- don't know how to put that in
+        daph_j*deathj - sat_fun(l,m,algae)*daph_j
+      
+        #daphnia adults are added from growth by juveniles and are lost from death
+      ddaph_a <- sat_fun(l,m,algae)*daph_j- daph_a*deatha
+        
+        #ceriodaphnia are added by birth and are lost from death
+      dcerio <- sat_fun(g,h,algae)*cerio - cerio*deathc
+     
+        # algae is added from growth based on uptake of ammonium and nitrate
+       dalgae <- algae*sat_fun(a,s,ammonium) + algae*sat_fun(p,q, nitrate) -
+        # algae is lost from being eaten by each of the 3 zooplankon
+         -daph_a*sat_fun(a,b,algae) + daph_j*sat_fun(c,d,algae)+cerio*sat_fun(e,f,algae) -
+         # density dependent death
+         algae*deatha
+      
+       
+        # return the rate of change
+         list(c(dammonium, dnitrate, ddaph_j,ddaph_a,dcerio,dalgae))
+       }) # end with(as.list ...
+   }
