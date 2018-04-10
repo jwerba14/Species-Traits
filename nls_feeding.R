7bf40e40ce1af211be7d7d8f8e8344e31ad4281c
jwerba14 <werbaj@mcmaster.ca>
2018-04-10 16:28:39 -0400

    fixing dataframes

--- nls_feeding.R
+++ nls_feeding.R
@@ -25,6 +25,12 @@ newpred <- hollings2(a= seq(0.1,24,0.1), h = 0.000122, r = 137.7)
 plot(seq(0.1,24,0.1), newpred)
 points(rdat1$chl1,rdat1$rate_chl_ind)
 
+#lit cerio
+#ldat <- read.csv("Feeding_lit_cerio.csv")
+# looks pretty linear...and with higher conc less filtration-- bc they are looking at amnt of water...
+#cm <- nls(ml.animal.hr_filtered~(concentration_algae.ug.ml.)/(1+concentration_algae.ug.ml.*h*r), start = list(h=.00001,r=-500), data = ldat )
+
+
 rdat2 <- read.csv("Ceriodaphnia_Feeding_Nov07_2017.csv")
 
 rdat3 <- filter(rdat2, Control.Y.N == "N", date == "11_7" | date == "11_16" | date == "12_31")
@@ -32,3 +38,17 @@ rdat3$rate_chl <- with(rdat3, (Chl.1-chl2)/Chl_Time_Diff)
 rdat3$rate_chl_ind <- with(rdat3, rate_chl/X..of.ceriodaphnia)
 
 m2 <- nls(rate_chl_ind~(Chl.1*h)/(1+Chl.1*h*r), start = list(h=0.00001,r=100), data = rdat3 )
+
+
+rdatj <- read.csv("Small_Daph_Feeding.csv")
+rdatj1 <- rdatj %>%
+  filter(Control.Y.N == "N", date == "10_4_2017"| date == "1_8_2017" | date == "2_12_2018")
+rdatj1$rate_chl <- with(rdatj1, (Chl.1-Chl.2)/Chl_Time_Diff)
+rdatj1$rate_chl_ind <- with(rdatj1, rate_chl/X..of.Daphnia)
+
+m3 <- nls(rate_chl_ind~(Chl.1*h)/(1+Chl.1*h*r), start = list(h=0.0001,r=130), data = rdatj1 )
+# h= .000026 p= 0.02 std er .000014; r = -448 se = 798 p = .57
+
+newpred <- hollings2(a= seq(0.1,24,0.1), h = 0.000026, r = -448)
+plot(seq(0.1,24,0.1), newpred)
+points(rdatj1$Chl.1,rdatj1$rate_chl_ind)
