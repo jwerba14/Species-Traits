d28a65d78a95a3776f31ee44a041398a625ca8be
Ben Bolker <bbolker@gmail.com>
2019-10-30 14:20:25 -0400

    bmb notes from meeting

--- Nitrogen_Algae/priors.R
+++ Nitrogen_Algae/priors.R
@@ -9,8 +9,20 @@ library(ggplot2); theme_set(theme_bw())
 library(ggstance)
 ## note: MASS::fitdistr vs. fitdistrplus::fitdist
 
+## trying to do things 'tidy':
+## use broom 'verbs':
+##  tidy gets coefficient tables
+##  glance gets model summary (AIC etc.)
+##  augment gets predictions (abusing this slightly)
+
+## tidy method for fitdistr actually works fine for 'fitdist' objects
 tidy.fitdist <- broom:::tidy.fitdistr
 
+##' @param x is a fitted object
+##' @param dname name of the distribution fitted, without "d" (e.g. "gamma", "lnorm")
+##' @param data either a numeric vector of the original data fitted, *or* a list or data frame with an "x" column (or the column specified by varname)
+##' @param varname column from data to use
+##' @param log (logical) return log-density instead of density?
 augment.fitdistr <- function(x, dname, data, n=101, varname="x", log=FALSE) {
     if (varname %in% names(data)) data <- data[[varname]]
     tibble(xval=seq(min(data, na.rm=TRUE),
@@ -32,6 +44,7 @@ augment.mle2 <- function(x, data, n=101, log=FALSE, dname=NULL) {
     augment.fitdistr(x, dname=dname, data=data, n=n, log=log)
 }
 
+## model summaries
 glance.fitdist <- function(x) {
     with(x,tibble(n=n,logLik=loglik,AIC=aic,BIC=bic))
 }
@@ -41,6 +54,8 @@ glance.mle2 <- function(x) {
            logLik=c(logLik(x)),AIC=AIC(x),BIC=BIC(x))
 }
 
+## helper function to use mle2 to fit a gamma, computing starting
+## values automatically
 mle2_fit_gamma <- function(x,...) {
     ## CV = 1/sqrt(shape)
     ## -> shape = 1/CV^2 = mean^2/var
@@ -60,6 +75,8 @@ mle2_fit_gamma <- function(x,...) {
 select <- dplyr::select
 litdat <- read.csv("literature_extraction.csv")
 
+## make all fits (all three packages x {gamma, lognormal}
+## for a specified variable
 make_fits <- function(x_name="half_sat_mg_N_L", data=litdat) {
     x <- c(na.omit(data[[x_name]]))
     ## c() drops attributes (fitdist doesn't like them)
@@ -94,6 +111,7 @@ make_fits <- function(x_name="half_sat_mg_N_L", data=litdat) {
                                fitdistrplus=fit5,
                                mle2=fit6))
 
+    ## attach the original data in case we want it later
     attr(fitList,"data") <- x
     return(fitList)
 }
@@ -101,7 +119,8 @@ make_fits <- function(x_name="half_sat_mg_N_L", data=litdat) {
 sum_fits <- function(fitList, y_max=NA, bins=30) {
     data <- attr(fitList,"data")
     ## parameters: map_dfr runs the function, then row-binds the results
-    params <- map_dfr(fitList,.id="dist",
+    params <- map_dfr(fitList,.id="distribution",
+                      ## run tidy for each package and combine
                       ~(map_dfr(.,tidy,.id="pkg")
                           %>% select(pkg,term,estimate,std.error)))
 
@@ -110,17 +129,17 @@ sum_fits <- function(fitList, y_max=NA, bins=30) {
         geom_pointrangeh(aes(xmin=estimate-2*std.error,
                              xmax=estimate+2*std.error),
                          position=position_dodgev(height=0.5))+
-        facet_wrap(~dist,ncol=1,scale="free")+
+        facet_wrap(~distribution,ncol=1,scale="free")+
         scale_color_brewer(palette="Dark2")
     
     ## goodness-of-fit (log-likelihood, AIC)
     gof <- map_dfr(fitList,
-                   .id="dist",
+                   .id="distribution",
                    ~(map_dfr(.,glance,.id="pkg")
                        %>% select(pkg,logLik,AIC)))
 
     ## predictions
-    pred <- map2_dfr(fitList,names(fitList),.id="dist",
+    pred <- map2_dfr(fitList,names(fitList),.id="distribution",
                      ~(map_dfr(.x,augment,dname=.y,
                                data=data,
                                .id="pkg")))
@@ -129,7 +148,7 @@ sum_fits <- function(fitList, y_max=NA, bins=30) {
         geom_histogram(data=data.frame(xval=data),
                        bins=bins,
                        aes(y=..density..))+
-        geom_line(aes(colour=dist,linetype=pkg),lwd=2)+
+        geom_line(aes(colour=distribution,linetype=pkg),lwd=2)+
         scale_y_continuous(limits=c(0,y_max))+
         scale_colour_brewer(palette="Dark2")
 
@@ -139,10 +158,9 @@ sum_fits <- function(fitList, y_max=NA, bins=30) {
 }
 
 
-
-
 half_sat_fits <- make_fits("half_sat_mg_N_L")
 half_sat_sum <- sum_fits(half_sat_fits,y_max=0.4)
+names(half_sat_sum)
 
 half_sat_sum$gof
 half_sat_sum$pred_plot
@@ -156,6 +174,10 @@ death_rate_day_fits <- make_fits("death_rate_day")
 death_rate_day_sum <- sum_fits(death_rate_day_fits,bins=10)
 death_rate_day_sum$pred_plot
 death_rate_day_sum$gof 
+
+## to do (?)
+##  add confidence intervals?
+##  we should probably be using AICc instead?
 
 ### end of BMB code
 
@@ -202,3 +224,5 @@ max_up <- litdat %>% dplyr::select(max_uptake_day) %>% filter(max_uptake_day !="
 max_up <- as.vector(max_up$max_uptake_day)
 ff5 <- fitdist(max_up, "gamma") #gamma(1.13,.12)
 denscomp(ff5)
+
+
